80c6031bb0c63396f931af4ecf2677b262406683
Hetsunya <99246859+Hetsunya@users.noreply.github.com>
2025-08-10 23:44:44 +0500

    на сервер

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from threading import Thread
 from apps.simulation_app.dashboard import TradingDashboard
 from apps.logs_app.logs_dashboard import LogsDashboard
+import yaml  # Импорт для чтения конфига
+
+# Загружаем конфиг
+config = yaml.safe_load(open('config.yaml', 'r'))
+env = config.get('env', 'prod')  # По умолчанию dev
 
 def run_simulation_app():
     app = TradingDashboard()
-    app.run(port=8060)
+    app.run(port=config['ports'][env]['simulation'])  # Порт из конфига
 
 def run_logs_app():
     app = LogsDashboard()
-    app.run(port=8061)
+    app.run(port=config['ports'][env]['logs'])  # Порт из конфига
 
 if __name__ == "__main__":
     t1 = Thread(target=run_simulation_app)
