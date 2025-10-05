1fb0dc8d4b0d789f7ffd13f30d5ec3941b7cf783
Hetsunya <99246859+Hetsunya@users.noreply.github.com>
2025-10-05 18:02:02 +0500

    новая версия с dev4 fivesec

--- utils/parser.py
+++ utils/parser.py
@@ -70,8 +70,8 @@ class TableParser:
                 pred_time = time_match.group()
 
                 predictions['5s'] = (pred_value, change_pct, pred_time)
-                # mae_text = cells[3].text.strip()
-                # mae_10min = float(mae_text) if mae_text != "..." else None  # Обработка "..."
+                mae_text = cells[3].text.strip()
+                mae_10min = float(mae_text) if mae_text != "..." else None  # Обработка "..."
 
             else:
                 min_pred_text = cells[2].text.strip().split('(')
